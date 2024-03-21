512aed251ef322a33093d6f30d7df6c675f116b4
Maxim Zheleznykovich <maxssmail572@gmail.com>
2024-03-21 20:22:09 +0300

    Initial commit

---
+++ src/main/java/com/example/Parnell/Agency/ParnellAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Parnell.Agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParnellAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParnellAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Parnell/Agency/ParnellAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Parnell.Agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParnellAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
